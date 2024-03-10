e392bc9b14504f6dd6403abfb1c5f97e792cc150
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-10 16:39:28 +0530

    lang slider with react-slick

--- src/app/components/HomeCard/HomeCard.jsx
+++ src/app/components/HomeCard/HomeCard.jsx
@@ -1,6 +1,87 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const HomeCard = () => {
+  const settings = {
+    // dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    loop: true,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1460,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1290,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+    ],
+  };
+
   return (
     <React.Fragment>
       <div
@@ -25,7 +106,6 @@ const HomeCard = () => {
                   <h1 className="text-[#223740] font-recoletaBlack text-5xl lg:text-7xl mt-5 md:mt-3">
                     I&rsquo;m Raphael
                   </h1>
-
                   <h2 className="text-[#223740] font-bold uppercase md:text-xl py-2">
                     Full Stack Software Developer
                   </h2>
@@ -50,6 +130,33 @@ const HomeCard = () => {
                 <div className="container m-auto">
                   <img src="ralph.png" decoding="async" alt="profile pic" />
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="container m-auto absolute">
+            <div
+              className="relative max-w-sm bottom-[70px] md:max-w-5xl xl:max-w-6xl px-5 lg:px-14 overflow-auto mx-auto bg-white rounded-2xl z-20"
+              style={{
+                boxShadow: "#48afde -7px 20px 50px 7px",
+              }}
+            >
+              <div className="sm:py-6 lg:py-10 cursor-all-scroll">
+                <Slider {...settings} className="ml-5">
+                  <img src="/js.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/css.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/html.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/react.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/redux.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/next.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/ruby.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/rails.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/psql.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/mongo.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/node.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/express.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/prisma.jpeg" alt="lang logo" className="h-12" />
+                  <img src="/mysql.jpeg" alt="lang logo" className="h-12" />
+                </Slider>
               </div>
             </div>
           </div>
