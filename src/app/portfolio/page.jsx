fb285c52a363441c1e23d9a846feae99ca4bb32b
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-13 23:14:04 +0530

    complete portfolio page.

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,11 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import data from "../assets/data/portfolioData";
+import Modal from "@/components/Modal/Modal";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import "remixicon/fonts/remixicon.css";
 
 const page = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   const [portfolios, setPortfolios] = useState(data);
   const [selectTab, setSelectTab] = useState("all");
-  console.log(portfolios);
+  const [showModal, setShowModal] = useState("false");
+  const [activeID, setActiveID] = useState("null");
+  const [nextItems, setNextItems] = useState(6);
 
   useEffect(() => {
     if (selectTab === "all") {
@@ -24,6 +33,15 @@ const page = () => {
       setPortfolios(filteredData);
     }
   }, [selectTab]);
+
+  const loadMoreHandler = () => {
+    setNextItems((prev) => prev + 3);
+  };
+
+  const showModalHandler = (id) => {
+    setShowModal(true);
+    setActiveID(id);
+  };
 
   return (
     <React.Fragment>
@@ -71,9 +89,49 @@ const page = () => {
                   </button>
                 </div>
               </div>
+              <div className="flex items-center gap-4 flex-wrap mt-12">
+                {portfolios?.slice(0, nextItems)?.map((portfolio, index) => (
+                  <div
+                    key={index}
+                    data-aos="fade-zoom-in"
+                    data-aos-delay="20"
+                    data-aos-duration="1000"
+                    className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]"
+                  >
+                    <figure>
+                      <img
+                        className="rounded-[8px]"
+                        src={portfolio.imgUrl}
+                        alt="portfolio"
+                      />
+                    </figure>
+                    <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5] hidden group-hover:block">
+                      <div className="w-full h-full flex items-center justify-center">
+                        <button
+                          onClick={() => showModalHandler(portfolio.id)}
+                          className="text-white bg-headingColor hover:bg-smallTextColor  py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+                        >
+                          See Details
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="text-center mt-6">
+                {nextItems < portfolios.length && data.length > 6 && (
+                  <button
+                    onClick={loadMoreHandler}
+                    className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+                  >
+                    Load more
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
+        {showModal && <Modal setShowModal={setShowModal} activeID={activeID} />}
       </div>
     </React.Fragment>
   );
