928dda53da5d11b2e59b9b4936d27d59522798e5
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-15 07:16:25 +0530

    bug fix

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import React,  { useEffect } from "react";
 
 const Contact = () => {
   useEffect(() => {
