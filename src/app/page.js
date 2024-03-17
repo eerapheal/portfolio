fc1dfba9931347561bf41c034101674f0a41c467
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-17 18:55:52 +0530

    refactored

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import HomeCard from "@/components/HomeCard/HomeCard";
+import Contact from "@/components/Contact/Contact";
+import Portfolio from "@/components/Portfolio/Portfolio";
+import About from "@/components/About/About";
 
 export default function Home() {
   return (
     <main className="">
       <HomeCard />
+      <Portfolio />
+      <About />
+      <Contact />
     </main>
   );
 }
